7bedaa62ffe01850d39a34e1a1c4b1d4f63e2e4a
jeikatsu <cjpfatalla@gmail.com>
2021-04-10 15:25:00 +0800

    added read me

--- Flutter/bayanihan_news/lib/main.dart
+++ Flutter/bayanihan_news/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bayanihan_news/ui/homeview.dart';
+import 'package:flutter_config/flutter_config.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FlutterConfig.loadEnvVariables();
   runApp(MyApp());
 }
 
