d97caca553adf4853df77ce1154902a073e4c259
jeikatsu <cjpfatalla@gmail.com>
2021-04-10 16:16:38 +0800

    fix twillio

--- Flutter/bayanihan_news/lib/main.dart
+++ Flutter/bayanihan_news/lib/main.dart
@@ -2,18 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bayanihan_news/ui/homeview.dart';
-import 'package:flutter_config/flutter_config.dart';
+// import 'package:flutter_config/flutter_config.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await FlutterConfig.loadEnvVariables();
+  // await FlutterConfig.loadEnvVariables();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // print(FlutterConfig.get('accountSid'));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
--- Flutter/bayanihan_news/lib/services/sms_service.dart
+++ Flutter/bayanihan_news/lib/services/sms_service.dart
@@ -1,10 +1,10 @@
 import 'package:twilio_flutter/twilio_flutter.dart';
-import 'package:flutter_config/flutter_config.dart';
+// import 'package:flutter_config/flutter_config.dart';
 
 final TwilioFlutter twilioFlutter = TwilioFlutter(
-    accountSid: FlutterConfig.get('accountSid'),
-    authToken: FlutterConfig.get('authToken'),
-    twilioNumber: FlutterConfig.get('twilioNumber'));
+    accountSid: "ACbe69f5a8fe6cb4bb3a2acdc2800c2b3c",
+    authToken: "df5b44e4553b4a9f85cf222154b34f43",
+    twilioNumber: "+13134665980");
 
 void sendSms(String phone, String message) async {
   twilioFlutter.sendSMS(toNumber: phone, messageBody: message);
